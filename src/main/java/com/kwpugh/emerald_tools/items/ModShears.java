69e279f43b42d3987c724425a64c9beda4667ef9
Kevin Pugh <kwpugh@comcast.net>
2021-07-11 12:25:10 -0500

    update

---
+++ src/main/java/com/kwpugh/emerald_tools/items/ModShears.java
@@ -0,0 +1,11 @@
+package com.kwpugh.emerald_tools.items;
+
+import net.minecraft.item.ShearsItem;
+
+public class ModShears extends ShearsItem
+{
+    public ModShears(Settings settings)
+    {
+        super(settings);
+    }
+}
