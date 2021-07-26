53dbffb3efb91e1f2bd8952da6dfeb2fc9e55235
Kevin Pugh <kwpugh@comcast.net>
2021-07-26 15:11:16 -0500

    update

--- src/main/java/com/kwpugh/emerald_tools/init/BlockInit.java
+++ src/main/java/com/kwpugh/emerald_tools/init/BlockInit.java
@@ -12,7 +12,10 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class BlockInit
-{	
+{
+	static boolean enableRuby = EmeraldTools.CONFIG.GENERAL.enableRubySet;
+	static boolean enableSteel = EmeraldTools.CONFIG.GENERAL.enableSteelSet;
+
 	public static final Block RUBY_BLOCK = new Block(FabricBlockSettings.of(Material.STONE).hardness(3.0F).resistance(3.0F));
 	public static final Block STEEL_BLOCK = new Block(FabricBlockSettings.of(Material.STONE).hardness(3.0F).resistance(3.0F));
 	
@@ -24,7 +27,14 @@ public class BlockInit
 	
 	public static void registerBlockItems()
 	{
-		Registry.register(Registry.ITEM, new Identifier(EmeraldTools.MOD_ID, "ruby_block"), new BlockItem(RUBY_BLOCK, new Item.Settings().group(EmeraldTools.EMERALD_TOOLS_GROUP)));
-		Registry.register(Registry.ITEM, new Identifier(EmeraldTools.MOD_ID, "steel_block"), new BlockItem(STEEL_BLOCK, new Item.Settings().group(EmeraldTools.EMERALD_TOOLS_GROUP)));
+		if(enableRuby)
+		{
+			Registry.register(Registry.ITEM, new Identifier(EmeraldTools.MOD_ID, "ruby_block"), new BlockItem(RUBY_BLOCK, new Item.Settings().group(EmeraldTools.EMERALD_TOOLS_GROUP)));
+		}
+
+		if(enableSteel)
+		{
+			Registry.register(Registry.ITEM, new Identifier(EmeraldTools.MOD_ID, "steel_block"), new BlockItem(STEEL_BLOCK, new Item.Settings().group(EmeraldTools.EMERALD_TOOLS_GROUP)));
+		}
 	}
 }
